3cddcb20b27a8bead53c0091444411a05b301152
Louis <louisr.daviaud@gmail.com>
2017-01-26 00:58:43 +0100

    Set with 1 element

---
+++ src/java/implementation/InnerSet_1.java
@@ -0,0 +1,50 @@
+package implementation;
+
+public class InnerSet_1<T> implements InnerSet<T>
+{	
+	private T element_0;
+	
+	public InnerSet_1()
+	{
+		
+	}
+	
+	public InnerSet_1(T p_element)
+	{
+		element_0 = p_element;
+	}
+
+	@Override
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public InnerSet<T>addElement(T p_element)
+	{
+		if(element_0.equals(p_element))
+			return this;
+		else
+			return new InnerSet_2(element_0, p_element);
+	}
+
+	@Override
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public InnerSet<T> removeElement(T p_element)
+	{
+		if(element_0.equals(p_element))
+				return new InnerSet_0();
+		else
+			return this;
+	}
+	
+	@Override
+	public String toString()
+	{
+		StringBuilder string_builder = new StringBuilder();
+		
+		string_builder.append("{");
+		
+		string_builder.append(element_0);
+		
+		string_builder.append("}");
+		
+		return string_builder.toString();
+	}
+}
