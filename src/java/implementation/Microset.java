cb83d24ff3d029344cb0bb46323fc64c1e15e97c
Louis <louisr.daviaud@gmail.com>
2017-01-26 01:00:09 +0100

    Container of InnerSet, to implement

---
+++ src/java/implementation/Microset.java
@@ -0,0 +1,102 @@
+package implementation;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MicroSet<T> implements Set<T>
+{
+	InnerSet<T> inner_set;
+
+	@Override
+	public boolean add(T e)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends T> c)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(Object o)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator<T> iterator()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object o)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object[] toArray()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
