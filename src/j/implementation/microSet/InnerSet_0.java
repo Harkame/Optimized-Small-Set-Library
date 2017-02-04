0eabda22cf4c53a0234fb8f1118d0e5ff219669b
Makgora <titouan.parand@outlook.fr>
2017-02-04 11:20:22 +0100

    Update MicroSet

--- src/j/implementation/microSet/InnerSet_0.java
+++ src/j/implementation/microSet/InnerSet_0.java
@@ -2,14 +2,9 @@ package j.implementation.microSet;
 
 public class InnerSet_0 <T> implements InnerSet<T>
 {
-	static InnerSet_0<Object> singleton = new InnerSet_0<Object>();
-
-	public InnerSet_0()
-	{
-	}
+	static InnerSet_0<Object> singleton = new InnerSet_0<>();
 	
 	@Override
-	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public InnerSet<T> addElement(T p_element)
 	{
 		return new InnerSet_1(p_element);
@@ -24,13 +19,7 @@ public class InnerSet_0 <T> implements InnerSet<T>
 	@Override
 	public String toString()
 	{
-		StringBuilder string_builder = new StringBuilder();
-		
-		string_builder.append("{");
-		
-		string_builder.append("}");
-		
-		return string_builder.toString();
+		return "{}";
 	}
 	
 	@Override
