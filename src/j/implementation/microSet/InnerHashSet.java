bf9c42e5138aada24a902dcb2758c293e4fbf890
Louis <louisr.daviaud@gmail.com>
2017-03-02 14:40:25 +0100

    Fix : extends HashSet and implements InnerSet

--- src/j/implementation/microSet/InnerHashSet.java
+++ src/j/implementation/microSet/InnerHashSet.java
@@ -4,43 +4,42 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 
+public class InnerHashSet<T> extends HashSet<T> implements InnerSet<T>
+{
+	private static final long serialVersionUID = 1L;
 
-public class InnerHashSet<T> implements InnerSet<T> {
-
-    HashSet<T> datas;
-
-    public InnerHashSet() {
-        this.datas = new HashSet<>();
+	public InnerHashSet() {
+        super();
     }
 
-    public InnerHashSet(InnerSet i) {
+    public InnerHashSet(InnerSet<T> i) {
         super();
-        Iterator it = i.getIterator();
+        Iterator<T> it = i.getIterator();
         while(it.hasNext()) {
-            this.datas.add((T) it.next());
+            add((T) it.next());
         }
     }
 
     @Override
     public InnerSet<T> addElement(T p_element) {
-        datas.add(p_element);
+        add(p_element);
         return this;
     }
 
     @Override
     public InnerSet<T> addAllElements(Collection<? extends T> p_newC) {
-        datas.addAll(p_newC);
-        return this;
+    	addAll(p_newC);
+    	return this;
     }
 
     @Override
     public boolean containsElement(Object p_element) {
-        return datas.contains(p_element);
+        return contains(p_element);
     }
 
     @Override
     public boolean containsAllElements(Collection<?> p_c) {
-        return datas.containsAll(p_c);
+        return containsAll(p_c);
     }
 
     @Override
@@ -49,29 +48,42 @@ public class InnerHashSet<T> implements InnerSet<T> {
     }
 
     public Iterator<T> getIterator() {
-        return datas.iterator();
+        return iterator();
     }
 
     @Override
     public InnerSet<T> removeElement(Object p_element) {
-        datas.remove(p_element);
+        remove(p_element);
         return this;
     }
 
     @Override
     public InnerSet<T> removeAllElements(Collection<?> p_cToDelete) {
-        datas.removeAll(p_cToDelete);
+        removeAll(p_cToDelete);
         return null;
     }
 
     @Override
     public InnerSet<T> retainAllElements(Collection<?> p_cToKeep) {
-        datas.retainAll(p_cToKeep);
+        retainAll(p_cToKeep);
         return this;
     }
 
     @Override
     public int getSize() {
         return 0;
+    }
+    
+    public static void main(String[] Args)
+    {
+    	InnerHashSet<Integer> inner_hash_set = new InnerHashSet<Integer>();
+    	System.out.println(inner_hash_set.toString());
+    	inner_hash_set.add(12);
+    	System.out.println(inner_hash_set.toString());
+    	inner_hash_set.add(42);
+    	System.out.println(inner_hash_set.toString());
+    	System.out.println(inner_hash_set.contains(42));
+    	inner_hash_set.remove(12);
+    	System.out.println(inner_hash_set.toString());
     }
 }
